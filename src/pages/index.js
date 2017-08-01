d0485a124d0e5c79c90912256eb1df2cb8b12673
saf0911 <saf0911@gmail.com>
2017-08-01 18:03:15 -0500

    just a push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-// import Link from 'next/link';
 import Header from '../components/Header';
+
+
 
 function IndexPage() {
   return (
     <div>
       <h1>
-        Hello World
+        Quick Patient Forms
       </h1>
       <Header />
+      
     </div>
   );
-  
+
 }
 
 export default IndexPage;
