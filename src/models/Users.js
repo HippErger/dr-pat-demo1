6fcb46c035d14d1c31ae13b244f889296460fa72
saf0911 <saf0911@gmail.com>
2018-05-18 11:05:58 -0500

    added Users model

---
+++ src/models/Users.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    maxlength: [ 30, 'something went wrong' ]
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    maxlength: [ 20, 'That is a long email' ]
+  },
+  phone: {
+    type: String,
+    maxlength: [ 20, 'Phone please' ]
+  },
+  message: {
+    type: String,
+    maxlength: [ 500, 'Leave some information to start the conversation' ]
+  },
+
+
+});
+
+export default mongoose.model('User', userSchema);
